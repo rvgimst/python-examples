9ef0d4e5ecff5da336d74e3f9b744fbd0699ae48
Ronald van Gimst <ronald@Frodo.fritz.box>
2017-01-16 23:21:33 +0100

    add koch snowflake

--- fractals.py
+++ fractals.py
@@ -1,28 +1,51 @@
+import math
 import turtle
 
 w = turtle.Screen()
+size = w.screensize()
+width = size[0]
+print(width)
 w.bgcolor("grey20")
 
 t = turtle.Turtle()
-t.color("blue")
+t.hideturtle()
+t.shape("turtle")
+t.color("yellow")
 t.pensize(2)
 t.speed(0)
 
-def f(distance, level):
-	if level == 0:
-		t.forward(distance)
-	else:
-		f(distance/3, level-1)
-		t.left(60)
-		f(distance/3, level-1)
-		t.right(120)
-		f(distance/3, level-1)
-		t.left(60)
-		f(distance/3, level-1)
+def koch(distance, level):
+    if level == 0:
+        t.forward(distance)
+    else:
+        koch(distance/3, level-1)
+        t.left(60)
+        koch(distance/3, level-1)
+        t.right(120)
+        koch(distance/3, level-1)
+        t.left(60)
+        koch(distance/3, level-1)
+
+
+def koch_snowflake(distance, level):
+    t.up()
+    t.setpos(-distance/2, (-distance * math.sqrt(3))/4)
+    t.down()
+    for angle in [-60,120,120]:
+        t.right(angle)
+        koch(distance, level)
+    t.setheading(0)
 
 t.up()
-t.backward(600)
+t.backward(width/2)
 t.down()
+koch(width,3)
 
-f(1200,5)
+t.up()
+t.setpos(0,0)
+t.setheading(0)
+t.down()
+t.color("lightblue")
+koch_snowflake(width,3)
+
 w.exitonclick()
